ad712f929291be55e0b557312c2a69a5cc38eaf4
Zsolt Varnai <zsolt.varnai@skyscanner.net>
2015-06-15 13:47:04 +0200

    [DOCSCRIPT] Deploying Documentation to gh-pages branch

--- documentation.sh
+++
@@ -1,1 +0,0 @@
-/usr/local/bin/appledoc --project-name Dixie --project-company Skyscanner --output "./Documentation/" --logformat xcode --exit-threshold \"2\" --keep-intermediate --verbose 4 "./Dixie/Dixie"
